7ec1287f8bf2fbfe5c94a6e97b08bc8465d557c2
Matei Leonard <leonard.matei2011@yahoo.com>
2024-02-10 13:01:36 +0200

    initial-commit/ create homepage, one component, setup prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { type ReactNode } from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@mui/material'
+import useSWR from 'swr'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BookRow from './components/custom-table-row/'
+
+interface Book {
+  id: string
+  title: string
+  author: string
+  description: string
 }
 
-export default App;
+const fetcher = async (url: string): Promise<Book[]> => {
+  const response = await axios.get(url)
+  return response.data
+}
+
+const MainPage: React.FC = (): ReactNode => {
+  const { data: books, error } = useSWR<Book[]>(
+    ' http://localhost:3001/books',
+    fetcher
+  )
+  console.log(books, error)
+
+  if (error) {
+    return <div>Error loading books</div>
+  }
+
+  return (
+    <div>
+      <h1>My Personal Library</h1>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell>Author</TableCell>
+              <TableCell>Description</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {books?.map((book) => (
+              <BookRow
+                key={book.id}
+                title={book.title}
+                author={book.author}
+                description={book.description}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  )
+}
+
+export default MainPage
