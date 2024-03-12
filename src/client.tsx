ca155c93bb4c4900bdaab0d30a29073d159cfd3b
Matei Leonard <leonard.matei2011@yahoo.com>
2024-03-12 16:34:34 +0200

    delete log to debugging

--- src/client.tsx
+++ src/client.tsx
@@ -4,15 +4,9 @@ import {
   type NormalizedCacheObject
 } from '@apollo/client'
 
-console.log(
-  'before initializing gql client',
-  process.env.REACT_APP_SERVER_DOMAIN
-)
-
 const client = new ApolloClient<NormalizedCacheObject>({
   uri: process.env.REACT_APP_SERVER_DOMAIN,
   cache: new InMemoryCache()
 })
-console.log('initialised')
 
 export default client
