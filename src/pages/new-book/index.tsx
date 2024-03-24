bbd34f3bab17c7f8377e174bd5acb52691671b7a
Matei Leonard <leonard.matei2011@yahoo.com>
2024-03-24 18:04:50 +0200

    refactor create and update book

--- src/pages/new-book/index.tsx
+++ src/pages/new-book/index.tsx
@@ -1,49 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import {
-  Container,
-  TextField,
-  TextareaAutosize,
-  Button,
-  Typography
-} from '@mui/material'
+import { Container, Typography } from '@mui/material'
 import { useMutation } from '@apollo/client'
 
 import { CREATE_BOOK } from '../../graphql/mutations'
 import { type FormData } from '../../types'
+import BookForm from '../../components/book-form'
 
 import './styles.scss'
 
 const CreateBookPage: React.FC = () => {
   const navigate = useNavigate()
-  const [formData, setFormData] = useState<FormData>({
-    title: '',
-    author: '',
-    description: ''
-  })
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ): void => {
-    const { name, value } = e.target
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value
-    }))
-  }
-
   const [createBookMutation] = useMutation(CREATE_BOOK)
 
-  const handleSubmit = async (e: React.FormEvent): Promise<void> => {
+  const handleSubmit = async (
+    e: React.FormEvent,
+    formData: FormData
+  ): Promise<void> => {
     e.preventDefault()
 
     await createBookMutation({
       variables: {
-        input: {
-          title: formData.title,
-          author: formData.author,
-          description: formData.description
-        }
+        input: formData
       }
     })
       .then(() => {
@@ -59,48 +37,7 @@ const CreateBookPage: React.FC = () => {
       <Typography variant='h4' gutterBottom>
         Create a New Book
       </Typography>
-      {/*  eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-      <form onSubmit={handleSubmit}>
-        <TextField
-          fullWidth
-          id='title'
-          name='title'
-          label='Title'
-          variant='outlined'
-          margin='normal'
-          value={formData.title}
-          onChange={handleChange}
-          required
-        />
-
-        <TextField
-          fullWidth
-          id='author'
-          name='author'
-          label='Author'
-          variant='outlined'
-          margin='normal'
-          value={formData.author}
-          onChange={handleChange}
-          required
-        />
-
-        <TextareaAutosize
-          id='description'
-          name='description'
-          aria-label='Description'
-          placeholder='Description'
-          minRows={3}
-          value={formData.description}
-          onChange={handleChange}
-          style={{ width: '100%', marginBottom: 16, resize: 'vertical' }}
-          required
-        />
-
-        <Button type='submit' variant='contained' color='primary'>
-          Create Book
-        </Button>
-      </form>
+      <BookForm handleSubmit={handleSubmit} buttonText='Create Book' />
     </Container>
   )
 }
